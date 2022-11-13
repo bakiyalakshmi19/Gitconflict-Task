27d58a589394027d9582f0a20a23042acd93658d
bakiyalakshmi19 <bakiyalakshmi111989@gmail.com>
2022-11-13 22:07:18 +0530

    Initial code

---
+++ src/test/java/org/hcl/GitTask.java
@@ -0,0 +1,17 @@
+package org.hcl;
+
+public class GitTask {
+
+	 private void test1() {
+		 System.out.println("test1");
+		
+	}
+	 private void test2() {
+		 System.out.println("test2");
+		
+	}
+	 private void test3() {
+		 System.out.println("test3");
+			
+	}
+}
